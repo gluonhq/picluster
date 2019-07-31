8ab18096e2561c17a12d514bb5d09e6c0aae022b
jose.pereda <jose.pereda@gluonhq.com>
2019-07-31 18:27:09 +0200

    Add background thread to update random chunks

--- DisplayApp/src/main/java/com/gluonhq/picluster/display/DisplayApp.java
+++ DisplayApp/src/main/java/com/gluonhq/picluster/display/DisplayApp.java
@@ -1,8 +1,8 @@
 package com.gluonhq.picluster.display;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,7 +11,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -26,6 +25,9 @@ public class DisplayApp extends Application {
     private static final int SIZE = 1024;
 
     private StackPane imagePane;
+    private GridPane gridPane;
+    private int SIZE_X, SIZE_Y;
+    private volatile boolean run;
 
     @Override
     public void start(Stage primaryStage) {
@@ -55,15 +57,22 @@ public class DisplayApp extends Application {
         primaryStage.setX(x);
         primaryStage.setY(y);
 
+        splitImage(imageView.snapshot(null, null));
         Button split = new Button("Split");
-        split.setOnAction(e -> splitImage(imageView.snapshot(null, null)));
+        split.setOnAction(e -> {
+            run = ! run;
+            if (run) {
+                processTask();
+            }
+
+        });
         StackPane.setAlignment(split, Pos.TOP_LEFT);
         imagePane.getChildren().add(split);
     }
 
     private void splitImage(Image fullImage) {
         PixelReader reader = fullImage.getPixelReader();
-        int SIZE_X = 0, SIZE_Y = 0;
+
         if (fullImage.getWidth() < fullImage.getHeight()) {
             SIZE_X = (int) Math.floor(Math.sqrt(SIZE));
             SIZE_Y = SIZE / SIZE_X;
@@ -73,31 +82,65 @@ public class DisplayApp extends Application {
         }
         int width = (int) (fullImage.getWidth() / SIZE_X);
         int height = (int) (fullImage.getHeight() / SIZE_Y);
-        GridPane pane = new GridPane();
+        gridPane = new GridPane();
         for (int i = 0; i < SIZE_X; i++) {
             for (int j = 0; j < SIZE_Y; j++) {
                 // TODO: Read opacity from server
-                double opacity = 0.5 + (double) new Random().nextInt(SIZE) / (double) (2d * SIZE);
-                
-                WritableImage wImage = processImageChunk(reader, width, height, i, j, opacity);
+//                double opacity = 0.5 + (double) new Random().nextInt(SIZE) / (double) (2d * SIZE);
+
+//                WritableImage wImage = processImageChunk(reader, width, height, i, j, opacity);
+                WritableImage wImage = new WritableImage(reader, i * width, j * height, width, height);
 
                 ImageView imageView = new ImageView(wImage);
                 imageView.setFitWidth(width);
                 imageView.setPreserveRatio(true);
-                pane.add(imageView, i, j);
+                gridPane.add(imageView, i, j);
             }
         }
-        imagePane.getChildren().setAll(new Group(pane));
+        imagePane.getChildren().setAll(new Group(gridPane));
     }
 
-    private WritableImage processImageChunk(PixelReader reader, int width, int height, int i, int j, double opacity) {
-        WritableImage newImage = new WritableImage(reader, i * width, j * height, width, height);
-        PixelReader pixelReader = newImage.getPixelReader();
-        WritableImage wImage = new WritableImage((int) newImage.getWidth(), (int) newImage.getHeight());
+    private void processTask() {
+        Thread thread = new Thread(() -> {
+            while (run) {
+                int chunkId_X = new Random().nextInt(SIZE_X);
+                int chunkId_Y = new Random().nextInt(SIZE_Y);
+                double opacity = 0.5 + (double) new Random().nextInt(SIZE) / (double) (2d * SIZE);
+                processImageChunk(chunkId_X, chunkId_Y, opacity);
+                try {
+                    Thread.sleep(10 + new Random().nextInt(10));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    private void processImageChunk(int i, int j, double opacity) {
+        Platform.runLater(() -> gridPane.getChildren().stream()
+                .filter(ImageView.class::isInstance)
+                .filter(n -> GridPane.getColumnIndex(n) == i &&
+                        GridPane.getRowIndex(n) == j)
+                .map(ImageView.class::cast)
+                .findFirst()
+//                .ifPresent(n -> n.setOpacity(opacity)));
+                .ifPresent(n -> {
+                    Image newImage = processImagePixels(n.getImage(), opacity);
+                    n.setImage(newImage);
+                }));
+    }
+
+    private WritableImage processImagePixels(Image chunk, double opacity) {
+        PixelReader pixelReader = chunk.getPixelReader();
+        int width = (int) chunk.getWidth();
+        int height = (int) chunk.getHeight();
+        WritableImage wImage = new WritableImage(width, height);
         PixelWriter pWriter = wImage.getPixelWriter();
 
-        for (int y = 0; y < newImage.getHeight(); y++) {
-            for (int x = 0; x < newImage.getWidth(); x++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 Color color = pixelReader.getColor(x, y);
                 Color newColor = Color.color(color.getRed(), color.getGreen(), color.getBlue(), opacity);
                 pWriter.setColor(x, y, newColor);
