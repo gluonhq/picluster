ca53dc19ea9ee85d5a76263e9efad2748816b794
Johan Vos <johan@lodgon.com>
2019-08-12 15:15:51 +0200

    added connection to DisplayService

--- IotWorker/src/main/java/com/gluonhq/picluster/iotworker/Main.java
+++ IotWorker/src/main/java/com/gluonhq/picluster/iotworker/Main.java
@@ -5,7 +5,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.util.Random;
 
 public class Main {
 
@@ -15,10 +18,13 @@ public class Main {
     static final String SERVER_IP = "127.0.0.1";
     static final int SERVER_PORT = 39265;
 
+    static final String DISPLAY_IP = "";
+    static final int DISPLAY_PORT = -1;
+
     static boolean go = true;
 
     public static void main(String[] args) {
-System.err.println("Hi, I'm an IoT worker");
+        System.err.println("Hi, I'm an IoT worker");
         try {
             runJobs();
         } catch (IOException e) {
@@ -38,20 +44,16 @@ System.err.println("Hi, I'm an IoT worker");
             String taskId = job.substring(0, idx);
             String url = job.substring(idx+1);
             System.err.println("Need to process "+url);
-            String answer = processURL(taskId, url);
+            int answer = processURL(taskId, url);
 
-            msg = ID+SEP+"answer"+SEP+answer+"\n";
-
+            msg = ID+SEP+"answer"+SEP+taskId+SEP+answer+"\n";
+            sendResultToDisplayApp(answer);
             sendSingleMessage(msg);
-//            bw.write(answer);
-//            bw.flush();
-//            bw.close();
-            // this is an URL
 
         }
     }
 
-    static String processURL (String taskId, String url) {
+    static int processURL (String taskId, String url) {
         System.err.println("Processing image at "+url+" with taskId "+taskId);
         try {
             Thread.sleep(5000);
@@ -59,7 +61,7 @@ System.err.println("Hi, I'm an IoT worker");
             e.printStackTrace();
         }
         int answer = (int)(Math.random()*100);
-        return taskId+SEP+answer;
+        return answer;
     }
 
     static String sendSingleMessage (String msg) throws IOException {
@@ -78,7 +80,36 @@ System.err.println("Hi, I'm an IoT worker");
         bw.close();
         s.close();
         return job;
+    }
 
+    private static final int PORT = 6666;
+    private static final int SIZE_X = 32;
+    private static final int SIZE_Y = 32;
+    private static final int SIZE = 1024;
+
+
+    static void sendResultToDisplayApp(int answer) {
+        try {
+            Socket clientSocket = new Socket(DISPLAY_IP, DISPLAY_PORT);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            int chunkId_X = new Random().nextInt(SIZE_X);
+            int chunkId_Y = new Random().nextInt(SIZE_Y);
+            double opacity = 100* answer;
+            String msg = String.format("%d,%d,%s", chunkId_X, chunkId_Y,
+                    String.format("%.2f", opacity).replace(",", "."));
+            out.println(msg);
+            String resp = null;
+            try {
+                resp = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (ConnectException c) {
+            System.out.println("Error connection: " + c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
