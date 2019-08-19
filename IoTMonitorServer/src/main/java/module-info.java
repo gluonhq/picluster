6c9d8b712ed8c8eee89bae6c7dfd41b904ec06ec
jose.pereda <jose.pereda@gluonhq.com>
2019-08-19 18:42:26 +0200

    Add jlink to iotMonitorServer

---
+++ IoTMonitorServer/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module iotserver {
+    requires javafx.controls;
+    requires eu.hansolo.tilesfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.material;
+    requires org.controlsfx.controls;
+
+    exports com.gluonhq.iotmonitor.monitor;
+}
