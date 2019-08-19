726a03b356befabf70b7d621222a2e11cce90643
jose.pereda <jose.pereda@gluonhq.com>
2019-08-19 19:49:17 +0200

    ClientApp that does URL connections to server

--- Client/src/main/java/com/gluonhq/picluster/client/ClientApp.java
+++ Client/src/main/java/com/gluonhq/picluster/client/ClientApp.java
@@ -1,87 +1,30 @@
 package com.gluonhq.picluster.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ConnectException;
-import java.net.Socket;
 import java.util.Random;
 
 public class ClientApp {
-    private static final int PORT = 6666;
-    private static final int SIZE_X = 32;
-    private static final int SIZE_Y = 32;
-    private static final int SIZE = 1024;
-
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-    static volatile boolean run;
-
+    static volatile boolean run = true;
 
     public void startConnection(String ip, int port) {
-        try {
-            clientSocket = new Socket(ip, port);
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            run = true;
-        } catch (ConnectException c) {
-            System.out.println("Error connection: " + c);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void updateChunks() {
-        while (run) {
-            int chunkId_X = new Random().nextInt(SIZE_X);
-            int chunkId_Y = new Random().nextInt(SIZE_Y);
-            double opacity = 0.5 + (double) new Random().nextInt(SIZE) / (2d * SIZE);
-            sendMessage(String.format("%d,%d,%s", chunkId_X, chunkId_Y,
-                    String.format("%.2f", opacity).replace(",", ".")));
+        while(run) {
             try {
-                Thread.sleep(10 + new Random().nextInt(20));
-            } catch (InterruptedException e) {
+                ProcessBuilder pb = new ProcessBuilder("curl", "http://" + ip + ":" + port + "/ID?text");
+                Process p = pb.start();
+                Thread.sleep(1000 + new Random().nextInt(100) * 10);
+            } catch (Exception e) {
+                run = false;
                 e.printStackTrace();
             }
         }
     }
 
-    public String sendMessage(String msg) {
-        if (out == null) {
-            return "No response";
-        }
-        out.println(msg);
-        String resp = null;
-        try {
-            resp = in.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return resp;
-    }
-
     public void stopConnection() {
-        try {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-            if (clientSocket != null) {
-                clientSocket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        run = false;
     }
 
     public static void main(String[] args) {
         ClientApp client = new ClientApp();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            run = false;
             client.stopConnection();
             try {
                 Thread.currentThread().join(5000);
@@ -89,7 +32,7 @@ public class ClientApp {
                 e.printStackTrace();
             }
         }));
-        client.startConnection("127.0.0.1", PORT);
-        client.updateChunks();
+        String ip = args.length == 1 ? args[0] : "192.168.68.107";
+        client.startConnection(ip, 8080);
     }
 }
