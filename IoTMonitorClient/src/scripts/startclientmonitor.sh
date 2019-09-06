97a78c35d96ee50fc6015ef0be786564be61afbc
jose.pereda <jose.pereda@gluonhq.com>
2019-09-06 20:19:33 +0200

    Use absolute path for class

--- IoTMonitorClient/src/scripts/startclientmonitor.sh
+++ IoTMonitorClient/src/scripts/startclientmonitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -XX:-UsePerfData -cp class com.gluonhq.iotmonitor.client.MainMonitor IP_MON_SERVER
-java -XX:-UsePerfData -cp class com.gluonhq.iotmonitor.iotworker.MainWorker IP_SERVER IP_DISPLAY
+java -XX:-UsePerfData -cp /mnt/nfs/var/nfsshare/pimonitorclientbundle/class com.gluonhq.iotmonitor.client.MainMonitor IP_MON_SERVER &
+java -XX:-UsePerfData -cp /mnt/nfs/var/nfsshare/pimonitorclientbundle/class com.gluonhq.picluster.iotworker.MainWorker IP_SERVER IP_DISPLAY
